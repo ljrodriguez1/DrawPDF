0f06835a9391017ebb0292e5893f0988de1c395f
Lucas <ljrodriguez1@uc.cl>
2020-05-29 15:38:00 -0400

    Updates

---
+++ precache-manifest.3e58e11e5e7dc21d0d6e47660d97e92e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef1412d2aaf94bfacda48a0eb66c814",
+    "url": "/DrawPDF/index.html"
+  },
+  {
+    "revision": "60c5caf0dc50aa148985",
+    "url": "/DrawPDF/static/css/main.230abce1.chunk.css"
+  },
+  {
+    "revision": "2d6b35462bede2501495",
+    "url": "/DrawPDF/static/js/2.bc3e0e99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DrawPDF/static/js/2.bc3e0e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c5caf0dc50aa148985",
+    "url": "/DrawPDF/static/js/main.aea8611d.chunk.js"
+  },
+  {
+    "revision": "3e20a0980690a5fc56ae",
+    "url": "/DrawPDF/static/js/runtime-main.fdb91a99.js"
+  }
+]);
